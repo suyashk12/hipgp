4c397d1e5d48cdf09f25df0d90de9b01c9d2d563
luhuan wu <>
2021-05-30 14:52:23 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="ziggy",
+      author="Luhuan Wu, Andrew Miller",
+      license="MIT",
+      version="1.0.0",
+      packages=['ziggy', 'ziggy.misc']
+      )
